2d4907a8d2ac5f5f8dcf7e0a960e20ed23732ac1
Maika Kailani <queenmab051@gmail.com>
2020-03-28 00:17:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,39 +1,28 @@
 class Artist
 
-  attr_accessor :name 
-  attr_reader :songs
-
-  @@song_count = []
+  attr_accessor :name, :songs
 
   def initialize(name)
     @name = name
     @songs = []
   end
 
-  def songs
-    @songs
-    add_song_by_name(song_name)
+  def add_song(song)
+    @songs << song
+    song.artist = self
   end
 
-  def songs
-    Song.all
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+    @songs << song
+  end
+
+  def self.song_count
+    Song.all.length
   end
 
   def songs
     Song.all.select {|song| song.artist == self}
-  end
-
-  def add_song(song)
-    @songs << songs
-    song.artist = self
-  end
-
-  def add_song_by_name(song_name)
-    song = Song.new(song_name)
-    add_song(song)
-  end
-
-  def self.song_count
-      Song.all.length
   end
 end
